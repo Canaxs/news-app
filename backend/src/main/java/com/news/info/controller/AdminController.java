0fbe62604a83910065881e6d16c88980b5a0a7c4
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-02-18 15:04:08 +0300

    security-minor changes

--- backend/src/main/java/com/news/info/controller/AdminController.java
+++ backend/src/main/java/com/news/info/controller/AdminController.java
@@ -2,7 +2,6 @@ package com.news.info.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +19,7 @@ public class AdminController {
 	
 	@PostMapping("/auth")
 	AdminAuthRes handleAuthentication(@RequestBody Credentials credentials) {
+		System.out.println(credentials);
 		return adminService.authenticate(credentials);
 	}
-	
-	
 }
--- backend/src/main/java/com/news/info/security/AdminSecurityConfiguration.java
+++ backend/src/main/java/com/news/info/security/AdminSecurityConfiguration.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +18,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.news.info.admin.token.AdminTokenFilter;
 
@@ -26,6 +29,7 @@ public class AdminSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
 			
 			@Override
@@ -35,8 +39,13 @@ public class AdminSecurityConfiguration extends WebSecurityConfigurerAdapter{
 				
 			}
 		});
-		http.headers().frameOptions().disable();
+		http
+		.authorizeRequests()
+		.and()
+		.authorizeRequests().anyRequest().permitAll();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(adminTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	@Bean
 	PasswordEncoder passwordEncoder() {
