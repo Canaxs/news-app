20e56648f10b009d0629008aba0e7aece906dbd8
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-02-27 21:30:30 +0300

    homepage-little-touches

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -3,6 +3,8 @@ import Navbar from '../fixedComponents/Navbar';
 import Slider from '../fixedComponents/Slider';
 import MenuCard from '../fixedComponents/MenuCard';
 import Link from '@mui/material/Link';
+import image1 from '../images/4.jpg';
+import image2 from '../images/5.jpg';
 
 const HomePage = () => {
   const body1 = {
@@ -22,9 +24,9 @@ const HomePage = () => {
         <Navbar />
         <Slider />
         <br />
-        <MenuCard body1={body1} color={'bg-info'} buttoncolor={'info'} aos={'fade-right'}/>
+        <MenuCard body1={body1} color={'bg-dark'} buttoncolor={'dark'} aos={'fade-right'} image={image1}/>
         <br />
-        <MenuCard body1={body2} color={'bg-dark'} buttoncolor={'dark'} aos={'fade-left'}/>
+        <MenuCard body1={body2} color={'bg-dark'} buttoncolor={'dark'} aos={'fade-left'} image={image2}/>
 
     </div>
   )
