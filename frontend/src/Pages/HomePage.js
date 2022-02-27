ed0a2daaad3809b74936052ebf174bf04cb62733
Meriç Cana <91698544+Canaxs@users.noreply.github.com>
2022-02-27 19:34:00 +0300

    menu-card-changes
    Changes have been made in the Menu Card. Menus have been added.

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -2,14 +2,29 @@ import React from 'react'
 import Navbar from '../fixedComponents/Navbar';
 import Slider from '../fixedComponents/Slider';
 import MenuCard from '../fixedComponents/MenuCard';
+import Link from '@mui/material/Link';
 
 const HomePage = () => {
+  const body1 = {
+    title: "Technology",
+    text1: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.Lorem İpsum Sİt Amet",
+    text2: "Last updated 3 mins ago"
+  }
+  const body2 = {
+    title: "Game",
+    text1: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.Lorem İpsum Sİt Amet",
+    text2: "Last updated 3 mins ago"
+  }
+  
+
   return (
     <div>
         <Navbar />
         <Slider />
         <br />
-        <MenuCard />
+        <MenuCard body1={body1} color={'bg-info'} buttoncolor={'info'}/>
+        <br />
+        <MenuCard body1={body2} color={'bg-dark'} buttoncolor={'dark'}/>
 
     </div>
   )
--- frontend/src/fixedComponents/MenuCard.js
+++ frontend/src/fixedComponents/MenuCard.js
@@ -1,26 +1,18 @@
 import React from 'react'
 import logo3 from '../images/3.jpg';
 
-const MenuCard = () => {
+const MenuCard = (props) => { 
+    const {body1,color,buttoncolor} = props;
   return (
-    <div className='ml-auto mr-auto d-flex w-100'>
-         <div className='w-50'>
-        <div className="card bg-dark text-white w-50 ml-auto mr-5">
-            <img className="card-img" src={logo3} alt="Card image" height='400' style={{objectFit:"cover"}}/>
-            <div className="card-img-overlay">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text">Last updated 3 mins ago</p>
-            </div>
-        </div>
-        </div>
-        <div className='w-50'>
-        <div className="card bg-dark text-white w-50 mr-auto ml-5 shadow ">
-            <img className="card-img" src={logo3} alt="Card image" height='400' style={{objectFit:"cover"}}/>
-            <div className="card-img-overlay">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text">Last updated 3 mins ago</p>
+    <div className='ml-auto mr-auto d-flex w-75'>
+         <div className='w-100 d-flex justify-content-center'>
+        <div className={`card ${color} text-white w-100`}>
+            <img className="card-img" src={logo3} alt="Card image" height='400' style={{objectFit:"cover",opacity:"70%"}}/>
+            <div className="card-img-overlay text-left">
+                <h3 className="card-title text-center">{body1.title}</h3>
+                <p className="card-text ">{body1.text1}</p>
+                <p className="card-text">{body1.text2}</p>
+                <button type="button" class={`btn btn-${buttoncolor} btn-lg`}>Go To Page</button>
             </div>
         </div>
         </div>
